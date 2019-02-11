2f458c4f051bafd620289b3055720e9cbc78ce56
yashlala <lala@graphite>
2019-02-11 14:37:21 -0800

    Automatically sync '~/io' to a temporary directory

---
+++ scripts/.local/scripts/make-io-a-tmpdir.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+tmpdir=$HOME/io
+# figure out later how to remove code redundancy with these bullshit
+# escaping rules
+
+if [ ! -e $tmpdir ] ; then
+	ln -sfn $(mktemp -d /tmp/io.XXX) $tmpdir
+	exit
+fi 
+
+if [ -L "$tmpdir" -a ! -e "$tmpdir" ] ; then
+	ln -sfn $(mktemp -d /tmp/io.XXX) $tmpdir
+	exit
+fi
+
+echo "Error: Cannot symlink '~/io' to a temporary directory." >/dev/stderr
+echo "       '~/io' exists and is not a broken symlink." >/dev/stderr
+exit 1
