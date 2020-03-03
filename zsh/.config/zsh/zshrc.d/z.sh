10cbdda558af4b2ff351c0b555b7fed621dd6414
yashlala <yashlala@gmail.com>
2020-03-02 18:51:51 -0800

    Move 'rupa/z.sh' alias to 'zshaliases'.

--- zsh/.config/zsh/zshrc.d/z.sh
+++ zsh/.config/zsh/zshrc.d/z.sh
@@ -1,6 +1,5 @@
 # source and configure the 'z' movement utility.
 if [ -r '/usr/share/z/z.sh' ]; then
-  _Z_CMD='f'
   _Z_EXCLUDE_DIRS=('/tmp')
   _Z_NO_RESOLVE_SYMLINKS=true
   _Z_DATA="$HOME/.cache/z-cache"
