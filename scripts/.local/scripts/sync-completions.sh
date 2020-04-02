de52f27a137c1606f94c94296b4cec83d2307def
Yash Lala <yashlala@gmail.com>
2020-04-01 18:33:02 -0700

    Conditionally update newly-synced completions.

--- scripts/.local/scripts/sync-completions.sh
+++ scripts/.local/scripts/sync-completions.sh
@@ -1,8 +1,17 @@
-#!/bin/sh
+#!/bin/sh -e
 
 fpath_folder=$HOME/.local/share/zsh/completions
 
 # TODO: check if files have changed before automatically writing them.
 
+update () { 
+  temp=$(mktemp)
+  eval $2 >"$temp"
+  if cmp -s "$temp" "$fpath_folder/$1"; then 
+    cp -f "$temp" "$fpath_folder/$1"
+  fi
+  rm "$temp"
+}
+
 # pip completions
-pip completion --zsh > "$fpath_folder/_pip"
+update _pip 'pip completion --zsh'
