af245faa69a4961c7377956dcb4df856e5e6bbce
Yash Lala <yashlala@gmail.com>
2020-04-09 20:46:44 -0700

    Fix: 'keychain.zsh' drop-in script not checking env vars.
    Not sure why this worked before.

--- zsh/.config/zsh/zshrc.d/keychain.zsh
+++ zsh/.config/zsh/zshrc.d/keychain.zsh
@@ -1,5 +1,8 @@
 # automatically start/connect to 'ssh-agent' as needed.
-if [ -n "$SSH_AGENT_SOCK" -a -n "$SSH_AGENT_PID" ]; then
+if [ -z "$SSH_AGENT_SOCK" ] || [ -z "$SSH_AGENT_PID" ]; then
+  if ! [ -d "$HOME/.cache/keychain" ]; then 
+    mkdir -p "$HOME/.cache/keychain"
+  fi
   eval $(keychain --noask --quiet --quick --absolute \
     --dir "$HOME/.cache/keychain" --eval)
 fi
