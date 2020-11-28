541420fd22484980f0af6345f9332a1cedde515d
Yash Lala <yashlala@gmail.com>
2020-11-27 17:49:29 -0800

    fix: Pass correct arguments through conda wrapper.

--- zsh/.config/zsh/zshrc.d/miniconda.zsh
+++ zsh/.config/zsh/zshrc.d/miniconda.zsh
@@ -1,6 +1,19 @@
 # Copied from `conda initialize`. 
 # Sets up wrapper commands around `conda`. 
 
+# We don't want conda to run all of its initialization code every single
+# time the shell opens, because it makes zsh very slow. 
+#
+# So instead, we wrap the initialization code in a function called "conda". 
+#
+# When we run "conda" for the first time in a shell session, this function will
+# load conda's initialization code, which will overwrite "conda" in the
+# shell namespace, replacing it with the true conda. Then, it runs conda. 
+#
+# In effect, this is "lazy loading". It might cause problems later on 
+# (eg. PATH doesn't include conda executables), but it works for now. 
+# Kludge 100. 
+
 function conda {
   __conda_setup="$('/home/lala/.local/share/miniconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
   if [ $? -eq 0 ]; then
@@ -14,6 +27,7 @@ function conda {
   fi
   unset __conda_setup
 
-  echo "Loaded conda as a shell function."
+
+  conda "$@"
 }
 # <<< conda initialize <<<
