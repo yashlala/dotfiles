fad3fecec309cc9c79ba14ed2b8e3a30f8959be3
Yash Lala <yashlala@gmail.com>
2021-10-23 09:19:41 -0700

    Fix: Lightline settings not copied to new `init.lua`.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -242,6 +242,17 @@ vim.g.indent_blankline_filetype_exclude = { 'markdown', 'text', 'asciidoc' }
 
 vim.api.nvim_set_var('lightline#bufferline#show_number', 2)
 vim.api.nvim_set_var('lightline#bufferline#unnamed', '[No Name]')
+vim.api.nvim_exec([[
+let g:lightline = {
+      \ 'colorscheme': 'seoul256',
+      \ 'active': {
+      \   'left': [ ['buffers'] ],
+      \   'right': [ [ 'lineinfo' ], [ 'percent' ],
+      \     [ 'fileformat', 'fileencoding', 'filetype' ] ]
+      \ },
+      \ 'component_expand': { 'buffers': 'lightline#bufferline#buffers' },
+      \ 'component_type': { 'buffers': 'tabsel' } }
+]], false)
 
 vim.g.go_doc_keywordprg_enabled = 0 -- disable K keybind.
 
