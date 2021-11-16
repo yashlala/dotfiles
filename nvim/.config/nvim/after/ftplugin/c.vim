ca7c06ebf029f56b77260031b4a09d48f027a915
Yash Lala <yashlala@gmail.com>
2021-11-16 11:22:43 -0800

    Rework nvim's entire `init.lua`.
    Many changes here, many partially applied. Not going into them now, but
    they seem to work.

--- nvim/.config/nvim/after/ftplugin/c.vim
+++ nvim/.config/nvim/after/ftplugin/c.vim
@@ -3,24 +3,4 @@ setlocal formatoptions+=tcrn1 formatoptions-=o
 setlocal cinoptions+=l1,:n1
 setlocal shiftwidth=4
 setlocal textwidth=79
-
-
-lua <<EOF
-
-require('lspconfig').ccls.setup({
-  init_options = {
-    --cache = {
-	--directory = "/home/lala/.cache/ccls";
-    --};
-    compilationDatabaseDirectory = "build";
-    index = {
-      threads = 0;
-    };
-    clang = {
-      excludeArgs = { "-frounding-math"};
-    };
-  }
-})
-
-
-EOF
+setlocal signcolumn=yes
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,19 @@
 -- Vanilla Vim Options
 
+-- TODO: Ask in the matrix room how to get highlights working. 
+-- Nothing we do here seems to work. Been changing up the colors etc. 
+-- We've installed fractal, a matrix client. use this to ask the developers in
+-- the matrix room. 
+vim.api.nvim_command('highlight HopNextKey  guifg=#00dfff gui=bold ctermfg=45 cterm=bold')
+vim.api.nvim_command('highlight HopNextKey1 guifg=#00dfff gui=bold ctermfg=45 cterm=bold')
+vim.api.nvim_command('highlight HopNextKey2 guifg=#2b8db3 ctermfg=33')
+vim.api.nvim_command('highlight HopUnmatched guifg=#666666 guibg=bg guisp=#666666 ctermfg=242')
+vim.api.nvim_command('highlight link HopCursor Cursor')
+vim.api.nvim_command('augroup HopInitHighlight')
+vim.api.nvim_command('autocmd!')
+vim.api.nvim_command("autocmd ColorScheme * lua require'hop.highlight'.insert_highlights()")
+vim.api.nvim_command('augroup end')
+
 vim.o.autochdir = true
 vim.o.autoindent = true
 vim.o.breakindent = true
@@ -20,6 +34,9 @@ vim.o.mouse = 'a'
 vim.o.scrollback = 1024
 vim.o.showmode = false
 vim.o.smartcase = true
+-- TODO: We just disabled this. Was it causing our weird highlight problems? 
+-- vim.o.termguicolors = true
+vim.o.termguicolors = false
 vim.o.undofile = true
 vim.o.wrap = true
 -- Swap digits and special characters. We need to do this in `langmap` (as
@@ -28,112 +45,14 @@ vim.o.wrap = true
 vim.o.langremap = false
 vim.o.langmap = '1!,!1,2@,@2,3#,#3,$4,4$,5%,%5,6^,^6,7&,&7,8*,*8,9(,(9,0),)0'
 
+-- Helper functions used in the keybinds above.
 
--- "Straightforward" Keymaps (there are more in the plugin setup funcs)
-
--- Vanilla Remaps
-
-do
-  vim.g.mapleader = ' '
-
-  local function mapwrap(mode, key, value, args)
-    vim.api.nvim_set_keymap(mode, key, value, args)
-  end
-  local function map(mode, key, value)
-    vim.api.nvim_set_keymap(mode, key, value, { noremap = false })
-  end
-  local function smap(mode, key, value)
-    vim.api.nvim_set_keymap(mode, key, value,
-      { noremap = false, silent = true })
-  end
-  local function noremap(mode, key, value)
-    mapwrap(mode, key, value, { noremap = true })
-  end
-  local function snoremap(mode, key, value)
-    mapwrap(mode, key, value, { noremap = true, silent = true })
-  end
-
-  snoremap('n', ' ', '<nop>')
-
-  snoremap('', 'Y', 'y$')
-  noremap('', ';', ':')
-  snoremap('', ':', 'q:')
-  noremap('', "'", '`')
-  noremap('', '\\', '"')
-  noremap('', '`', '~')
-  noremap('', '-', '0')
-  noremap('', ';', ':')
-  snoremap('', ':', 'q:')
-  snoremap('n', '<cr>', ':put _<cr>')
-  noremap('n', '<bar>', 'K')
-  noremap('n', 'K', 'kJ')
-  noremap('n', 'U', '<c-r>')
-  noremap('n', '<c-r>', 'U')
-  noremap('n', 'v', 'V')
-  snoremap( 'n', 'S', [[<cmd>keepp s/\\s*\%#\\s*/\\r/e <bar> norm! ==<CR>]])
-  snoremap('', '&', '<cmd>&&<cr>')
-  noremap('', '_', '<c-y>')
-  noremap('', '+', '<c-e>')
-  snoremap('n', '<c-n>', '<cmd>bnext<cr>')
-  snoremap('n', '<c-p>', '<cmd>bprev<cr>')
-  noremap('n', 'gs', ':%s/')
-  map('', 'ga', '<Plug>(EasyAlign)')
-  -- Simple Leader Keybinds
-  snoremap('', '<leader>p', '"0p')
-  snoremap('', '<leader>P', '"0P')
-  smap('n', '<leader>d', '<cmd>call DeleteBufferAndUpdateLightline()<cr>')
-  smap('n', '<leader>gg', '<cmd>G<cr>')
-  smap('n', '<leader>o', '<cmd>silent !uwin<cr>')
-  -- Terminal Mode Keybindings
-  noremap('t', '<c-\\>', '<c-\\><c-n>')
-  noremap('n', '<c-\\>', '<nop>')
-  noremap('i', '<c-\\>', '<esc>')
-  -- EasyMotion Keybindings
-  noremap('', 'f', '<nop>')
-  map('', 'f', '<Plug>(easymotion-f)')
-  map('', 'F', '<Plug>(easymotion-F)')
-  map('', 't', '<Plug>(easymotion-t)')
-  map('', 'T', '<Plug>(easymotion-T)')
-  map('', 's', '<Plug>(easymotion-lineanywhere)')
-  map('', '<leader>j', '<Plug>(easymotion-j)')
-  map('', '<leader>k', '<Plug>(easymotion-k)')
-  -- Diary Keybinds
-  map('n', '<leader>ww', '<Plug>VimwikiMakeDiaryNote')
-  map('n', '<leader>wi', '<cmd>DiaryIndex<cr>')
-  map('n', '<leader>wt', '<Plug>VimwikiMakeTodayDiaryNote')
-  map('n', '<leader>wy', '<Plug>VimwikiMakeYesterdayDiaryNote')
-  map('n', '<leader>w<leader>w', '<Plug>VimwikiIndex')
-  -- Quick Buffer Switching Keybindings
-  map('n', '<leader>!', '<Plug>lightline#bufferline#go(1)')
-  map('n', '<leader>@', '<Plug>lightline#bufferline#go(2)')
-  map('n', '<leader>#', '<Plug>lightline#bufferline#go(3)')
-  map('n', '<leader>$', '<Plug>lightline#bufferline#go(4)')
-  map('n', '<leader>%', '<Plug>lightline#bufferline#go(5)')
-  map('n', '<leader>^', '<Plug>lightline#bufferline#go(6)')
-  map('n', '<leader>&', '<Plug>lightline#bufferline#go(7)')
-  map('n', '<leader>*', '<Plug>lightline#bufferline#go(8)')
-  map('n', '<leader>(', '<Plug>lightline#bufferline#go(9)')
-end
-
--- Helper functions used in keybinds above.
-
-vim.api.nvim_exec([[
+vim.api.nvim_command([[
   function! DeleteBufferAndUpdateLightline()
     exe 'bdelete'
     call lightline#update()
   endfunction
-]], false)
---  Visit the diary index with :Diary. It'll regenerate
--- every time we visit it (updates itself with new links).
-vim.api.nvim_exec([[
-command! DiaryIndex VimwikiDiaryIndex
-augroup vimwikigroup
-    autocmd!
-    " automatically update links on read diary
-    autocmd BufRead,BufNewFile diary.wiki VimwikiDiaryGenerateLinks
-augroup end
-]], false)
-
+]])
 
 -- Plugins
 
@@ -145,80 +64,88 @@ if vim.fn.empty(vim.fn.glob(packer_install_path)) > 0 then
   .. packer_install_path)
 end
 -- Re-compile packer after any edits to our config file.
-vim.api.nvim_exec(
-  [[
+vim.api.nvim_exec([[
   augroup Packer
     autocmd!
     autocmd BufWritePost init.lua PackerCompile
     autocmd BufWritePost init.vim PackerCompile
   augroup end
-]], false)
+]], true)
 
 -- Load plugins.
 local use = require('packer').use
 require('packer').startup(function()
   -- Package manager.
-  use 'wbthomason/packer.nvim' 
+  use 'wbthomason/packer.nvim'
   -- Fancier statusline.
   use 'itchyny/lightline.vim'
   -- Buffer list for statusline.
   use 'mengelbrecht/lightline-bufferline'
   -- Indentation guides.
   use 'lukas-reineke/indent-blankline.nvim'
-  -- Colorscheme. 
-  use 'junegunn/seoul256.vim' 
-
-  -- Automatic table alignment.
-  use 'junegunn/vim-easy-align'
-  -- Surround vim motions. 
-  use 'tpope/vim-surround' 
-  -- "gc" keybinding to comment visual regions/lines.
-  use 'tpope/vim-commentary'
-  -- Date increments and decrements w/ <c-a> and <c-x>
-  use 'tpope/vim-speeddating' 
-  -- Unix commands. 
-  use 'tpope/vim-eunuch' 
- -- Automatically match file format/indentation.
-  use 'tpope/vim-sleuth'
-  -- Vim motions that don't require counts. 
-  use 'easymotion/vim-easymotion'
+  -- Colorscheme.
+  use 'junegunn/seoul256.vim'
 
   -- Diary + Wiki
   use 'vimwiki/vimwiki'
 
+ -- Automatically match file format/indentation.
+  use 'tpope/vim-sleuth'
+  -- Vim motions that don't require counts. 
+  use 'easymotion/vim-easymotion'
+  use 'phaazon/hop.nvim'
+
+  -- Automatic table alignment.
+  use 'junegunn/vim-easy-align'
+  -- Surround vim motions.
+  use 'tpope/vim-surround'
+  -- "gc" keybinding to comment visual regions/lines.
+  use 'tpope/vim-commentary'
+  -- Date increments and decrements w/ <c-a> and <c-x>
+  use 'tpope/vim-speeddating'
+  -- Unix commands.
+  use 'tpope/vim-eunuch'
+  use 'itchyny/calendar.vim'
   -- Git commands + Status page.
   use 'tpope/vim-fugitive'
   -- Git related info in signs column and popups.
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use { 'lewis6991/gitsigns.nvim', requires = 'nvim-lua/plenary.nvim' }
+  -- TODO: Set this up later. 
+  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
 
-  -- Remove whitespace at the end of modified lines when lives are saved.
-  use 'axelf4/vim-strip-trailing-whitespace'
+  -- File Browser
+  -- TODO: Set up.
+
+  -- Highlighting
+  -- use { 'kyazdani42/nvim-tree.lua',
+  --   requires = { 'kyazdani42/nvim-web-devicons' }
+  -- }
 
   -- Collection of configurations for built-in LSP client
   use 'neovim/nvim-lspconfig'
-  -- use 'WhoIsSethDaniel/toggle-lsp-diagnostics.nvim'
+  use 'WhoIsSethDaniel/toggle-lsp-diagnostics.nvim'
 
-  -- TODO: Enable Treesitter functionality. Has to be done explicitly. 
-  -- Highlighting, editing, etc. using incremental parsing. 
+  -- TODO: Enable Treesitter functionality. Has to be done explicitly.
+  -- Highlighting, editing, etc. using incremental parsing.
   -- use 'nvim-treesitter/nvim-treesitter'
   -- use 'L3MON4D3/LuaSnip' -- Snippets plugin. TODO: broken.
+  -- Autocompletion plugin
+  -- use 'hrsh7th/nvim-compe'
 
-  -- Global Menu and Fuzzy Finder. 
+  -- Global Menu and Fuzzy Finder.
   use { 'nvim-telescope/telescope.nvim', requires = {
     { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } }
   }
   use { 'nvim-telescope/telescope-fzy-native.nvim', requires = {
-    { 'nvim-telescope/telescope.nvim' } } 
+    { 'nvim-telescope/telescope.nvim' } }
   }
-  -- Automatically `cd` to project root. Integrates with Telescope. 
+  -- Automatically `cd` to project root. Integrates with Telescope.
   -- Use this to quickly return to old projects (as opposed to searching *in*
-  -- a project, which we do with the regular telescope builtins. 
+  -- a project, which we do with the regular telescope builtins.
   use "ahmedkhalf/project.nvim"
 
-  -- Autocompletion plugin
-  -- use 'hrsh7th/nvim-compe'
 
-  -- Allow plugins to use the `.` key (if they support it). 
+  -- Allow plugins to use the `.` key (if they support it).
   use 'tpope/vim-repeat'
 end)
 
@@ -226,10 +153,17 @@ end)
 --[[
 Kinkier Configuration
 --]]
+-- TODO: Set up a function that'll let us toggle on and off.
+-- In the meantime, just use this to disable the virtual text.
+require'toggle_lsp_diagnostics'.init({
+  start_on = true,
+  underline = true,
+  virtual_text = false
+})
 
 -- Load colorscheme *before* other plugins are set up.
-vim.api.nvim_exec('colorscheme seoul256', false)
 vim.g.seoul256_srgb = 1
+vim.api.nvim_command('colorscheme seoul256')
 
 vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_show_first_indent_level = false
@@ -250,37 +184,21 @@ let g:lightline = {
 ]], false)
 
 vim.api.nvim_exec([[
-let g:vimwiki_list = [{'path': '~/documents/vimwiki/',
-                      \ 'syntax': 'markdown', 'ext': '.md'}]
+let g:vimwiki_list = [{'path': '~/documents/vimwiki/', 'syntax': 'markdown', 
+      \ 'auto_diary_index': 1, 'auto_generate_links': 1, 'ext': '.md'}]
 ]], false)
 
 vim.g.go_doc_keywordprg_enabled = 0 -- disable K keybind.
 
 vim.g.EasyMotion_do_mapping = 0
 vim.g.EasyMotion_smartcase = 1
+require('hop').setup()
+
 
 require('project_nvim').setup({ manual_mode = true })
 require('telescope').setup {}
 require('telescope').load_extension('fzy_native')
 require('telescope').load_extension('projects')
-
-vim.api.nvim_set_keymap('n', '<leader>b',
-  "<cmd>lua require('telescope.builtin').buffers({})<cr>",
-  { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ff',
-  "<cmd>lua require('telescope.builtin').find_files({})<cr>",
-  { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fg',
-  "<cmd>lua require('telescope.builtin').grep_string({})<cr>",
-  { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fp',
-  "<cmd>lua require('telescope.builtin').git_files({})<cr>",
-  { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fo',
-  "<cmd>lua require('telescope.builtin').oldfiles({})<cr>",
-  { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fP',
-  "<cmd>Telescope projects<cr>", { noremap = true, silent = true })
 
 -- Git Plugin Setup
 
@@ -288,10 +206,10 @@ require('gitsigns').setup {
   keymaps = {
     -- Default keymap options
     noremap = true,
-    ['n ]g'] = { expr = true,
+    ['n ]c'] = { expr = true,
       "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"
     },
-    ['n [g'] = { expr = true,
+    ['n [c'] = { expr = true,
       "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"
     },
     ['n <leader>gs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
@@ -314,17 +232,33 @@ require('gitsigns').setup {
 
 -- Autocomplete + LSP Setup
 
--- Set up LSP Configurations.
-
 -- LSP Configurations are moved to the filetype-specific `after` configs.
+-- EDIT: no they aren't. for some reason they don't work when we put them
+
+require('lspconfig').ccls.setup({
+  init_options = {
+    cache = {
+	directory = "/home/lala/.cache/ccls";
+    };
+    compilationDatabaseDirectory = "build";
+    index = {
+      threads = 1;
+    };
+    clang = {
+      excludeArgs = { "-frounding-math"};
+    };
+  }
+})
+
+-- TODO: jdtls exits with error
+--require('lspconfig').jdtls.setup{}
+-- there.
 -- require('lspconfig').pyright.setup({})
-require('lspconfig').ccls.setup({})
 -- require('lspconfig').bashls.setup({})
 -- require('lspconfig').texlab.setup({})
 -- require('lspconfig').dockerls.setup({})
 -- require('lspconfig').gopls.setup({})
 -- require('lspconfig').jedi_language_server.setup{}
--- require('lspconfig').jdtls.setup{}
 
 --[[
 -- Setup `compe`.
@@ -451,7 +385,7 @@ autocmd BufNewFile,BufRead neomutt-* set filetype=mail
 -- Close netrw buffers (we can't do this by default, for some reason).
 vim.api.nvim_exec("autocmd Filetype netrw setl bufhidden=delete", false)
 
--- Enter terminal mode as soon as we enter a terminal buffer.
+-- Enter terminal mode as soon as we create a terminal buffer.
 vim.api.nvim_exec("autocmd TermOpen * startinsert", false)
 
 -- The bottom status line doesn't update very often, so the buffer listing can
@@ -461,3 +395,136 @@ vim.api.nvim_exec([[
 autocmd BufEnter,BufLeave,BufWritePost,BufHidden,BufWinEnter,BufWinLeave,CmdlineEnter,InsertEnter *
   \ call lightline#update()
 ]], false)
+
+
+
+--[[
+-- Keymaps
+--]]
+
+-- "Straightforward" Keymaps (there are more in the plugin setup funcs)
+
+-- Vanilla Remaps
+
+do
+  vim.g.mapleader = ' '
+
+  local function mapwrap(mode, key, value, args)
+    vim.api.nvim_set_keymap(mode, key, value, args)
+  end
+  local function map(mode, key, value)
+    vim.api.nvim_set_keymap(mode, key, value, { noremap = false })
+  end
+  local function smap(mode, key, value)
+    vim.api.nvim_set_keymap(mode, key, value,
+      { noremap = false, silent = true })
+  end
+  local function noremap(mode, key, value)
+    mapwrap(mode, key, value, { noremap = true })
+  end
+  local function snoremap(mode, key, value)
+    mapwrap(mode, key, value, { noremap = true, silent = true })
+  end
+
+  snoremap('n', ' ', '<nop>')
+
+  snoremap('', 'Y', 'y$')
+  noremap('', ';', ':')
+  snoremap('', ':', 'q:')
+  noremap('', "'", '`')
+  noremap('', '\\', '"')
+  noremap('', '`', '~')
+  noremap('', '-', '0')
+  noremap('', ';', ':')
+  snoremap('', ':', 'q:')
+  -- TODO: Add this functionality to all other keys when on a terminal window. 
+  -- I guess we'll have to pass in something to the script? 
+  noremap('n', '<bar>', 'K')
+  noremap('n', 'K', 'kJ')
+  noremap('n', 'U', '<c-r>')
+  noremap('n', '<c-r>', 'U')
+  noremap('n', 'v', 'V')
+  snoremap( 'n', 'S', [[<cmd>keepp s/\\s*\%#\\s*/\\r/e <bar> norm! ==<CR>]])
+  snoremap('', '&', '<cmd>&&<cr>')
+  noremap('', '_', '<c-y>')
+  noremap('', '+', '<c-e>')
+  snoremap('n', '<c-n>', '<cmd>bnext<cr>')
+  snoremap('n', '<c-p>', '<cmd>bprev<cr>')
+  noremap('n', 'gs', ':%s/')
+  map('', 'ga', '<Plug>(EasyAlign)')
+  -- Simple Leader Keybinds
+  snoremap('', '<leader>p', '"0p')
+  noremap('n', '<bar>', 'K')
+  noremap('n', 'K', 'kJ')
+  noremap('n', 'U', '<c-r>')
+  noremap('n', '<c-r>', 'U')
+  noremap('n', 'v', 'V')
+  snoremap( 'n', 'S', [[<cmd>keepp s/\\s*\%#\\s*/\\r/e <bar> norm! ==<CR>]])
+  snoremap('', '&', '<cmd>&&<cr>')
+  noremap('', '_', '<c-y>')
+  noremap('', '+', '<c-e>')
+  snoremap('n', '<c-n>', '<cmd>bnext<cr>')
+  snoremap('n', '<c-p>', '<cmd>bprev<cr>')
+  noremap('n', 'gs', ':%s/')
+  map('', 'ga', '<Plug>(EasyAlign)')
+  -- Simple Leader Keybinds
+  snoremap('', '<leader>p', '"0p')
+  snoremap('', '<leader>P', '"0P')
+  smap('n', '<leader>d', '<cmd>call DeleteBufferAndUpdateLightline()<cr>')
+  smap('n', '<leader>gg', '<cmd>G<cr>')
+  smap('n', '<leader>o', '<cmd>silent !uwin<cr>')
+  -- Terminal Mode Keybindings
+  noremap('t', '<c-\\>', '<c-\\><c-n>')
+  noremap('n', '<c-\\>', '<nop>') -- prevent current mode confusion
+  noremap('i', '<c-\\>', '<esc>') -- just keep mashing, we'll get to normal
+  -- EasyMotion Keybindings
+  noremap('', 'f', '<nop>')
+  map('', 'f', '<Plug>(easymotion-f)')
+  map('', 'F', '<Plug>(easymotion-F)')
+  map('', 't', '<Plug>(easymotion-t)')
+  map('', 'T', '<Plug>(easymotion-T)')
+  map('', 's', '<Plug>(easymotion-lineanywhere)')
+  map('', '<leader>j', '<Plug>(easymotion-j)')
+  map('', '<leader>k', '<Plug>(easymotion-k)')
+  -- LSP Keybinds
+  -- TODO
+  -- Diary Keybinds
+  map('n', '<leader>ww', '<Plug>VimwikiMakeDiaryNote')
+  map('n', '<leader>wi', '<Plug>VimwikiDiaryIndex')
+  map('n', '<leader>wt', '<Plug>VimwikiMakeTomorrowDiaryNote')
+  map('n', '<leader>wy', '<Plug>VimwikiMakeYesterdayDiaryNote')
+  map('n', '<leader>w<leader>w', '<Plug>VimwikiIndex')
+  -- Quick Buffer Switching Keybindings
+  map('n', '<leader>!', '<Plug>lightline#bufferline#go(1)')
+  map('n', '<leader>@', '<Plug>lightline#bufferline#go(2)')
+  map('n', '<leader>#', '<Plug>lightline#bufferline#go(3)')
+  map('n', '<leader>$', '<Plug>lightline#bufferline#go(4)')
+  map('n', '<leader>%', '<Plug>lightline#bufferline#go(5)')
+  map('n', '<leader>^', '<Plug>lightline#bufferline#go(6)')
+  map('n', '<leader>&', '<Plug>lightline#bufferline#go(7)')
+  map('n', '<leader>*', '<Plug>lightline#bufferline#go(8)')
+  map('n', '<leader>(', '<Plug>lightline#bufferline#go(9)')
+  -- Telescope Keybinds
+  snoremap('n', ',',
+      "<cmd>lua require('telescope.builtin').buffers({})<cr>")
+  snoremap('n', '<leader>ff',
+      "<cmd>lua require('telescope.builtin').find_files({})<cr>")
+  snoremap('n', '<leader>fg',
+      "<cmd>lua require('telescope.builtin').grep_string({})<cr>")
+  snoremap('n', '<leader>fp',
+      "<cmd>lua require('telescope.builtin').git_files({})<cr>")
+  snoremap('n', '<leader>fo',
+      "<cmd>lua require('telescope.builtin').oldfiles({})<cr>")
+  snoremap('n', '<leader>fP', "<cmd>Telescope projects<cr>")
+  -- LSP Keybinds
+  snoremap('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<cr>')
+  snoremap('n', '<leader>lD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+  snoremap('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+  snoremap('n', '<bar>', '<cmd>lua vim.lsp.buf.hover()<cr>')
+  map('n', '<leader>tl', '<Plug>(toggle-lsp-diag-default)')
+end
+
+-- TODO: 
+-- Create an alias: finally alias 'e' in the shell. 'e file' will edit the file
+-- in the CONTAINING nvim instance (if we use it in the nvim terminal, the
+-- outer nvim will open it! genius!)
