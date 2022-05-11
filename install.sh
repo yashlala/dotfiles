6cd36b3ca79e0efe4bd609ffba8be0513b0dae35
Yash Lala <yashlala@gmail.com>
2022-05-11 10:56:41 -0700

    Make `install.sh` more robust.

--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
 #!/bin/sh -e
 
-echo "What's the install command?"
+echo "This script installs a portable subset of the dotfiles."
+echo "You'll have to set up a full installation yourself."
+echo 
+echo "What's this machine's install command?"
 echo "(empty => \"don't install anything\")"
+echo "> "
 
 read -r installer
-if [ -n "$installer" ]; do 
+if [ -n "$installer" ]; then
   sudo $installer stow zsh
-done
+  find . -mindepth 1 -maxdepth 1 -type d -not -name '.git' \
+    | xargs -n 1 basename \
+    | xargs stow
+else
+  mkdir -p ~/.config/
+  cp -r ./zshrc/.config/zsh ~/.config/zsh
+  cp -r ./git/.config/git ~/.config/git
+fi
 
-find . -mindepth 1 -maxdepth 1 -type d -not -name '.git' \
-  | xargs -n 1 basename \
-  | xargs stow
-
+ln -s ~/.config/zshrc/zshrc ~/.zshrc
 rm ~/.config/zsh/zshrc.d/fasd.zsh ~/.config/zsh/zshrc.d/keychain.zsh
 
-chsh -s /bin/zsh 
+if [ -e /bin/zsh ]; then 
+  chsh -s /bin/zsh 
+else
+  echo "\nset -o vi" > ~/.bashrc
+fi
+
+echo "Reload shell to see changes."
