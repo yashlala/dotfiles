b53a2956f4f94741707d7f2eb453713ed0a0fece
Yash Lala <yashlala@gmail.com>
2022-06-25 23:12:28 -0700

    nvim: Don't set up CCLS for *.c files.
    Prior to this commit, we used both CCLS and Clangd as LSP servers for
    *.c and *.cpp files. Now, neovim throws an error when CCLS is enabled
    (`:LspInfo` fails with an internal "expected nil, got string").
    
    Disabling CCLS also seemed to fix our "multiple client offset" issues, so
    let's do that permanently. Clangd works just fine for our use cases.

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -10,23 +10,6 @@ vim.bo.textwidth = 79
 vim.api.nvim_command("setlocal formatoptions+=tcrn1 formatoptions-=o")
 vim.api.nvim_command("setlocal cinoptions+=l1,:n1")
 
--- Set up the CCLS language server.
-require('lspconfig').ccls.setup({
-  init_options = {
-    single_file_support = true,
-    cache = {
-	directory = "/home/lala/.cache/ccls";
-    };
-    compilationDatabaseDirectory = "build";
-    index = {
-      threads = 1;
-    };
-    clang = {
-      excludeArgs = { "-frounding-math"};
-    };
-  }
-})
-
 -- Set up Clangd for good measure.
 -- It gives us single file support, and is backed by Google etc.
 require'lspconfig'.clangd.setup{}
