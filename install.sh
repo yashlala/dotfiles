a711a871e59763c02775e1e114f3cacc3fd50ca4
Yash Lala <yashlala@gmail.com>
2022-08-03 12:55:34 -0700

    Tweak `install.sh`, better `lf` support.

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 
 echo "This script installs a portable subset of the dotfiles."
-echo "You'll have to set up a full installation yourself."
+echo "You'll have to set up full installations yourself."
+echo 
+echo "Make sure that you know this user's password!"
 echo 
 echo "What's this machine's install command?"
 echo "(empty => \"don't install anything\")"
@@ -32,11 +34,13 @@ else
   cp -r ./git/.config/git ~/.config/git
   cp ./readline/.config/readline/inputrc ~/.inputrc
   cp ./editline/.config/editline/config ~/.editrc
+  mkdir -p ~/.config/lf/lfrc && cp ./lf/.config/lf/lfrc ~/.config/lf/lfrc
 fi
 
 ln -s ~/.config/zsh/zshrc ~/.zshrc
 ln -s ~/.config/zsh/zshenv ~/.zshenv
 
+test -e ~/.config/lf/lfrc && sed -iE 's/nvr -s/vim/g'
 sed -iE '/export MANPAGER/d' ~/.config/zsh/zshenv
 sed -i 's/\/code\/bin/\/bin/g' ~/.config/zsh/zshenv
 rm ~/.config/zsh/zshrc.d/fasd.zsh \
@@ -44,11 +48,13 @@ rm ~/.config/zsh/zshrc.d/fasd.zsh \
   ~/.config/zsh/zshrc.d/miniconda.zsh
 
 if [ -x /bin/zsh ]; then 
+  echo 'Changing shell to /bin/zsh'
   chsh -s /bin/zsh 
 else
+  echo '/bin/zsh not available, sticking with bash :('
   printf '\nset -o vi\n' > ~/.bashrc
 fi
 
 touch ~/.hushlogin
 
-echo "Reload shell to see changes."
+echo 'Reload shell to see changes.'
