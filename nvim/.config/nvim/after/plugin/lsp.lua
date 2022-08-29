c3af2d6b36096b369170121ba1ca0f6090860702
Yash Lala <yashlala@gmail.com>
2022-08-29 14:20:48 -0700

    Move "toggle_lsp_diagnostics" plugin setup to LSP file.

--- nvim/.config/nvim/after/plugin/lsp.lua
+++
@@ -1,7 +0,0 @@
--- TODO: Set up a function that'll let us toggle on and off.
--- In the meantime, just use this to disable the virtual text.
-require('toggle_lsp_diagnostics').init({
-  start_on = true,
-  underline = true,
-  virtual_text = false
-})
--- nvim/.config/nvim/lua/yashlala/lsp.lua
+++ nvim/.config/nvim/lua/yashlala/lsp.lua
@@ -41,3 +41,11 @@ require'lspconfig'.sumneko_lua.setup {
     },
   },
 }
+
+-- TODO: Set up a function that'll let us toggle on and off.
+-- In the meantime, just use this to disable the virtual text.
+require('toggle_lsp_diagnostics').init({
+  start_on = true,
+  underline = true,
+  virtual_text = false
+})
