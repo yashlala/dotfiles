24380c4075fe3a43d3e8642ef49cb43459486293
Yash Lala <yashlala@gmail.com>
2023-01-26 14:48:56 -0800

    fix: incorrectly documented nvim mapping.

--- nvim/.config/nvim/lua/yashlala/mappings.lua
+++ nvim/.config/nvim/lua/yashlala/mappings.lua
@@ -197,7 +197,7 @@ M.setup = function()
         direction = require'hop.hint'.HintDirection.AFTER_CURSOR,
         hint_offset = -1,
       })
-    end, { desc = 'Delete backwards (exclusive)' })
+    end, { desc = 'Delete forwards (exclusive)' })
   vim.keymap.set('o', 'T', function()
       require('hop').hint_char1({
         direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,
