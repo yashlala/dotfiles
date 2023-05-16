92a14c02d65d30a48993c8f02bacba8a70394b7a
Yash Lala <yashlala@gmail.com>
2023-05-16 15:38:14 -0700

    fix(nvim): LessSpace causes problems w/ VimWiki bulleted lists.

--- nvim/.config/nvim/plugin/lessspace.lua
+++ nvim/.config/nvim/plugin/lessspace.lua
@@ -1,2 +1,4 @@
 vim.g.lessspace_normal = 0
-vim.g.lesspace_whitelist = { 'text', 'mail', 'vimwiki', 'markdown' }
+
+-- LessSpace causes problem with VimWiki's bulleted lists. 
+vim.g.lesspace_whitelist = { 'text', 'mail', 'markdown' }
