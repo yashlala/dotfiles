1ee82a6c26bda3afa281e37ab10b040d83812ebf
Yash Lala <yashlala@gmail.com>
2023-05-17 09:57:09 -0700

    fix(nvim): lessspace whitelists/blacklists don't work.
    This is why you don't have a variable name like "lessspace_*". One
    missed "s" and you're done.

--- nvim/.config/nvim/plugin/lessspace.lua
+++ nvim/.config/nvim/plugin/lessspace.lua
@@ -1,4 +1,4 @@
 vim.g.lessspace_normal = 0
 
 -- LessSpace causes problem with VimWiki's bulleted lists. 
-vim.g.lesspace_whitelist = { 'text', 'mail', 'markdown' }
+vim.g.lessspace_blacklist = { 'text', 'mail', 'vimwiki', 'markdown' }
