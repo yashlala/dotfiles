ae467bb18e5df7d059e138e94b6df8220a067959
Yash Lala <yashlala@gmail.com>
2023-05-31 13:20:15 -0700

    Disable lessspace.nvim for buffers w/ no filetype.
    Ideally we want to only enable this for "code" buffers (messes w/
    pasting). But without a comprehensive list of code filetypes, how should
    we do so?

--- nvim/.config/nvim/plugin/lessspace.lua
+++ nvim/.config/nvim/plugin/lessspace.lua
@@ -1,4 +1,4 @@
 vim.g.lessspace_normal = 0
 
--- LessSpace causes problem with VimWiki's bulleted lists. 
-vim.g.lessspace_blacklist = { 'text', 'mail', 'vimwiki', 'markdown' }
+-- LessSpace causes problems with VimWiki's bulleted lists.
+vim.g.lessspace_blacklist = { '', 'text', 'mail', 'vimwiki', 'markdown' }
