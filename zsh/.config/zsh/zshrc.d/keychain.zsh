a5b7c5c7450bc50f814b8e377c7fc412d0935963
Yash Lala <ylala@nvidia.com>
2025-06-23 16:38:09 -0700

    use macos keychain instead of `keychain` tool.

--- zsh/.config/zsh/zshrc.d/keychain.zsh
+++
@@ -1,8 +0,0 @@
-# automatically start/connect to 'ssh-agent' as needed.
-if [ -z "$SSH_AGENT_SOCK" ] || [ -z "$SSH_AGENT_PID" ]; then
-  if ! [ -d "$HOME/.cache/keychain" ]; then 
-    mkdir -p "$HOME/.cache/keychain"
-  fi
-  eval $(keychain --noask --quiet --quick --absolute \
-    --dir "$HOME/.cache/keychain" --eval)
-fi
